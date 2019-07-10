38717baf032c1b908627656dc976b0c658492a6c
Md. Shariful Islam <shaarifulz@gmail.com>
2019-07-10 12:34:15 +0600

    Generate Excel file On LocalDisk

---
+++ ExcellGenerate/src/main/java/com/exam/Contacts.java
@@ -0,0 +1,17 @@
+package com.exam;
+
+public class Contacts {
+	public String firstName;
+	public String lastName;
+	public String email;
+	public String dateOfBirth;
+	
+	public Contacts(String firstName, String lastName, String email, String dateOfBirth) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.dateOfBirth = dateOfBirth;
+	}
+	
+	
+}
